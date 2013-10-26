2025c6a82e753595950b1fd99dbc7d0101b9b436
Shawn Neal <sneal@sneal.net>
2013-10-26 12:00:39 -0700

    Make version configurable from CI

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'sneal@daptiv.com'
 license          'Apache 2.0'
 description      'Installs/Configures Visual Studio 2012'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version (ENV['BUILD_NUMBER'] ? "0.0.#{ENV['BUILD_NUMBER']}" : '0.1.0')
 depends          'windows'
 depends          '7-zip'
