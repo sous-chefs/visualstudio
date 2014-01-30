7c385f9053e2a446d66adc5d871904a316c148eb
Juan Laube <juanlaube@icloud.com>
2014-01-30 11:37:17 -0200

    Download vsto installer from Microsoft, small fixes in install_vsto recipe.

--- recipes/install_vsto.rb
+++ recipes/install_vsto.rb
@@ -1,9 +1,9 @@
 #
-# Author:: Joe Fitzgerald
+# Author:: Juan Laube
 # Cookbook Name:: visualstudio
-# Recipe:: installupdate
+# Recipe:: install_vsto
 #
-# Copyright 2013, Joe Fitzgerald.
+# Copyright 2014, Juan laube.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,22 +18,27 @@
 # limitations under the License.
 #
 
-# Ensure 7-zip is installed
-include_recipe '7-zip::default'
-
 # Ensure the installation ISO url has been set by the user
-install_source = node['visualstudio']['source']
-raise "'visualstudio source attribute must be set to run this cookbook" if install_source.nil?
-
-install_filename = node['visualstudio']['vsto']['filename']
-install_url = win_friendly_path(File.join (install_source, install_filename))
+package_src_url = node['visualstudio']['vsto']['package_src_url']
+temp_dir = win_friendly_path(File.join(Dir.tmpdir(), 'vs2012_vsto'))
+setup_exe_path = File.join(temp_dir, node['visualstudio']['vsto']['installer_file'])
 
 # Create install paths
 package_name = node['visualstudio']['vsto']['package_name']
 install_dir = node['visualstudio']['install_dir']
 install_log_file = win_friendly_path(File.join(install_dir, 'vstoinstall.log'))
+vs_is_installed = File.exists?(File.join(install_dir, '\Common7\IDE\devenv.exe'))
 
-setup_exe_path = File.join(iso_extraction_dir, node['visualstudio']['update']['installer_file'])
+# Create temp directory for package installer
+directory temp_dir do
+  action :create
+  not_if { vs_is_installed }
+end
+
+# Transfer package installer from Microsoft download site to local filesystem
+remote_file setup_exe_path do
+  source package_src_url
+end
 
 # Install Visual Studio 2012 Update
 windows_package package_name do
@@ -41,4 +46,9 @@ windows_package package_name do
   installer_type :custom
   options "/Q /norestart /noweb /Log \"#{install_log_file}\""
   action :install
+end
+
+# Remove temp directory
+directory temp_dir do
+  action :delete
 end
