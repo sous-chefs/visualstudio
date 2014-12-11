211498101672446f6bd1851a4c386d0a353ba9c1
ian <ian@asperasoft.com>
2014-12-11 12:37:58 -0800

    Add support for multiple editions (2010 tested so far)

--- libraries/helper.rb
+++ libraries/helper.rb
@@ -1,17 +1,25 @@
 module Visualstudio
   module Helper
 
-    def is_vs_installed?()
-      File.exists?(File.join(node['visualstudio']['install_dir'], '\Common7\IDE\devenv.exe'))
+    def is_vs_installed?(install_dir=nil)
+      install_dir ||= File.join(node['visualstudio']['install_dir']
+      File.exists?(File.join(install_dir, '\Common7\IDE\devenv.exe'))
     end
 
-    def is_sp_installed?()
-      registry_key_exists?(node['visualstudio']['update']['package_regkey'])
+    def is_sp_installed?(package_regkey = nil)
+      package_regkey ||= node['visualstudio']['update']['package_regkey']
+      registry_key_exists?(package_regkey)
     end
 
     def is_vsto_installed?()
       registry_key_exists?(node['visualstudio']['vsto']['package_regkey'])
     end
 
+    def attribute_from_edition_or_cookbook(edition, value)
+      # return either the edition specific, or, failing that, cookbook-wide attribute
+      attribute = node['visualstudio'][edition][value] || node['visualstudio'][value]
+      raise "attribute #{value} not found for #{edition}" unless attribute
+      attribute
+    end
   end
 end
--- recipes/install.rb
+++ recipes/install.rb
@@ -20,51 +20,58 @@
 
 ::Chef::Recipe.send(:include, Visualstudio::Helper)
 
-vs_is_installed = is_vs_installed?()
 
-# Ensure the installation ISO url has been set by the user
-if !node['visualstudio']['source']
-  raise 'visualstudio source attribute must be set before running this cookbook'
-end
+[*(node['visualstudio']['edition'])].each do |edition|
+  install_dir = attribute_from_edition_or_cookbook(edition, 'install_dir')
+  vs_is_installed = is_vs_installed?(install_dir)
 
-edition = node['visualstudio']['edition']
-install_url = File.join(node['visualstudio']['source'], node['visualstudio'][edition]['filename'])
-install_log_file = win_friendly_path(
-  File.join(node['visualstudio']['install_dir'], 'vsinstall.log'))
+  source = attribute_from_edition_or_cookbook(edition, 'source')
+  install_url = File.join(source, node['visualstudio'][edition]['filename'])
+  install_log_file = win_friendly_path(File.join(install_dir, 'vsinstall.log'))
 
-iso_extraction_dir = win_friendly_path(File.join(Chef::Config[:file_cache_path], 'vs2012'))
-setup_exe_path = File.join(iso_extraction_dir, node['visualstudio'][edition]['installer_file'])
-admin_deployment_xml_file = win_friendly_path(File.join(iso_extraction_dir, 'AdminDeployment.xml'))
+  iso_extraction_dir = win_friendly_path(
+    File.join(Chef::Config[:file_cache_path], 'vs2012', edition))
+  setup_exe_path = File.join(iso_extraction_dir, node['visualstudio'][edition]['installer_file'])
+  configure_basename = node['visualstudio'][edition]['config_file'] || 'AdminDeployment.xml'
+  configuration_file = win_friendly_path(File.join(iso_extraction_dir, configure_basename))
 
-# Extract the ISO image to the tmp dir
-seven_zip_archive 'extract_vs2012_iso' do
-  path iso_extraction_dir
-  source install_url
-  overwrite true
-  checksum node['visualstudio'][edition]['checksum']
-  not_if { vs_is_installed }
-end
+  # Extract the ISO image to the tmp dir
+  seven_zip_archive 'extract_vs2012_iso' do
+    path iso_extraction_dir
+    source install_url
+    overwrite true
+    checksum node['visualstudio'][edition]['checksum']
+    not_if { vs_is_installed }
+  end
 
-# Create installation config file
-cookbook_file admin_deployment_xml_file do
-  source 'AdminDeployment-' + edition + '.xml'
-  action :create
-  not_if { vs_is_installed }
-end
+  conf_source = node['visualstudio'][edition]['config_file'] ||
+    'AdminDeployment-' + edition + '.xml'
 
-# Install Visual Studio
-windows_package node['visualstudio'][edition]['package_name'] do
-  source setup_exe_path
-  installer_type :custom
-  options "/Q /norestart /Log \"#{install_log_file}\" /AdminFile \"#{admin_deployment_xml_file}\""
-  notifies :delete, "directory[#{iso_extraction_dir}]"
-  timeout 3600 # 1hour
-  not_if { vs_is_installed }
-end
+  # Create installation config file
+  cookbook_file configuration_file do
+    source conf_source
+    action :create
+    not_if { vs_is_installed }
+  end
 
-# Cleanup extracted ISO files from tmp dir
-directory iso_extraction_dir do
-  action :nothing
-  recursive true
-  not_if { node['visualstudio']['preserve_extracted_files'] }
+  Install Visual Studio
+  windows_package node['visualstudio'][edition]['package_name'] do
+    source setup_exe_path
+    installer_type :custom
+    if node['visualstudio'][edition]['config_file'] =~ /unattend/
+      options "/Q /norestart /Log \"#{install_log_file}\" /unattendfile \"#{configuration_file}\""
+    else
+      options "/Q /norestart /Log \"#{install_log_file}\" /AdminFile \"#{configuration_file}\""
+    end
+    notifies :delete, "directory[#{iso_extraction_dir}]"
+    timeout 3600 # 1hour
+    not_if { vs_is_installed }
+  end
+
+  # Cleanup extracted ISO files from tmp dir
+  directory iso_extraction_dir do
+    action :nothing
+    recursive true
+    not_if { node['visualstudio']['preserve_extracted_files'] }
+  end
 end
