adc58538033851e7de744eede5f853611b460986
ian <ian@asperasoft.com>
2015-07-28 13:33:45 -0700

    Remove helper functions, moved to providers

--- libraries/helper.rb
+++ libraries/helper.rb
@@ -1,25 +1,7 @@
 module Visualstudio
   module Helper
-
-    def is_vs_installed?(install_dir=nil)
-      install_dir ||= File.join(node['visualstudio']['install_dir'])
-      File.exists?(File.join(install_dir, '\Common7\IDE\devenv.exe'))
-    end
-
-    def is_sp_installed?(package_regkey = nil)
-      package_regkey ||= node['visualstudio']['update']['package_regkey']
-      registry_key_exists?(package_regkey)
-    end
-
     def is_vsto_installed?()
       registry_key_exists?(node['visualstudio']['vsto']['package_regkey'])
-    end
-
-    def attribute_from_edition_or_cookbook(edition, value)
-      # return either the edition specific, or, failing that, cookbook-wide attribute
-      attribute = node['visualstudio'][edition][value] || node['visualstudio'][value]
-      raise "attribute #{value} not found for #{edition}" unless attribute
-      attribute
     end
   end
 end
