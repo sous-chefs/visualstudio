6b7f4c7920ccd1d31245b99daf3516a4d9be9eb9
Linus Ruth <linus.ruth@changepoint.com>
2018-05-03 16:42:48 -0700

    chore: increment cookbook minor version in metadata

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ chef_version     '>= 12.6' if respond_to?(:chef_version)
 license          'Apache-2.0'
 description      'Installs/Configures Visual Studio'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.0'
+version          '1.3.0'
 supports         'windows'
 depends          'windows'
 depends          'seven_zip'
