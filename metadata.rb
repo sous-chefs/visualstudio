0e95688dff291aaa31923967753f8ec445b06ac4
Linus Ruth <linus.ruth@changepoint.com>
2018-05-10 15:33:10 -0700

    chore: update metadata to set supported chef >=13.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Shawn Neal'
 maintainer_email 'sneal@sneal.net'
 source_url       'https://github.com/windowschefcookbooks/visualstudio'
 issues_url       'https://github.com/windowschefcookbooks/visualstudio/issues'
-chef_version     '>= 12.6' if respond_to?(:chef_version)
+chef_version     '>= 13.0' if respond_to?(:chef_version)
 license          'Apache-2.0'
 description      'Installs/Configures Visual Studio'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
